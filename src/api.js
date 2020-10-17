076da18ea3ba47ca7d330559d2832cc2a684abe8
Frances <francesannlee7@gmail.com>
2020-10-17 18:00:41 +1100

    m1

--- src/api.js
+++ src/api.js
@@ -3,10 +3,20 @@
  * @param {*} path The url to make the reques to.
  * @param {*} options Additiona options to pass to fetch.
  */
-const getJSON = (path, options) => 
-    fetch(path, options)
-        .then(res => res.json())
-        .catch(err => console.warn(`API_ERROR: ${err.message}`));
+const getJSON = (path, options) => {
+    return new Promise((resolve, reject) => {
+        fetch(path, options)
+            .then(response => {
+                if (response.status === 200) {
+                    resolve(response.json());
+                } else {
+                    response.text().then(decoded => {
+                        reject("hello " + decoded["message"]);
+                    }).catch(err => console.log('uh oh' + err));
+                }
+            })
+    })
+}
 
 /**
  * This is a sample class API which you may base your code on.
@@ -19,7 +29,35 @@ export default class API {
     } 
 
     /** @param {String} path */
-    makeAPIRequest(path) {
-        return getJSON(`${this.url}/${path}`);
+    makeAPIRequest(path, options) {
+        return getJSON(`${this.url}/${path}`, options);
+    }
+
+    post(path, options) {
+        return getJSON(`${this.url}/${path}`, {
+            ...options,
+            method: 'POST',
+        });
+    }
+
+    put(path, options) {
+        return getJSON(`${this.url}/${path}`, {
+            ...options,
+            method: 'PUT',
+        });
+    }
+
+    get(path, options) {
+        return getJSON(`${this.url}/${path}`, {
+            ...options,
+            method: 'GET',
+        });
+    }
+
+    delete(path, options) {
+        return getJSON(`${this.url}/${path}`, {
+            ...options,
+            method: 'DELETE',
+        });
     }
 }
--- src/main.js
+++ src/main.js
@@ -4,8 +4,94 @@ import { fileToDataUrl } from './helpers.js';
 
 // This url may need to change depending on what port your backend is running
 // on.
-const api = new API('http://localhost:5000');
+const api = new API('http://localhost:5000'); // will need to change
 
 // Example usage of makeAPIRequest method.
-api.makeAPIRequest('dummy/user')
-    .then(r => console.log(r));
+
+const loginModal = document.getElementById("loginModal");
+const loginButton = document.getElementById("navLogin");
+const registerModal = document.getElementById("registerModal");
+const registerButton = document.getElementById("navRegister");
+const loginClose = document.getElementsByClassName("close")[0];
+const registerClose = document.getElementsByClassName("close")[1];
+
+loginButton.addEventListener("click", e => {
+    loginModal.style.display = "block";
+})
+document.getElementById("loginSubmit").addEventListener("click", e => {
+    console.log("hello");
+    e.preventDefault();
+    const loginUsername = document.getElementById("loginUsername").value;
+    const loginPassword = document.getElementById("loginPassword").value;
+    api.post("auth/login", {
+        body: JSON.stringify({
+            "username": loginUsername,
+            "password": loginPassword,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then (data => {
+            console.log(data);
+            document.getElementById("navNotLogged").style.display = "none";
+            document.getElementById("navLogged").style.display = "block";
+            document.getElementById("notLoggedIn").style.display = "none";
+            document.getElementById("loggedIn").style.display = "block";
+        })
+        .catch(err => {
+            alert(err);
+        })
+    console.log("woop woop")
+})
+
+registerButton.addEventListener("click", e => {
+    registerModal.style.display = "block";
+})
+document.getElementById("registerSubmit").addEventListener("click", e => {
+    e.preventDefault();
+    const registerUsername = document.getElementById("registerUsername").value;
+    const registerPassword = document.getElementById("registerPassword").value;
+    const registerEmail = document.getElementById("registerEmail").value;
+    const registerName = document.getElementById("registerName").value;
+    api.post("auth/signup", {
+        body: JSON.stringify({
+            "username": registerUsername,
+            "password": registerPassword,
+            "email": registerEmail,
+            "name": registerName,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then( () => {
+            document.getElementById("navNotLogged").style.display = "none";
+            document.getElementById("navLogged").style.display = "block";
+            document.getElementById("notLoggedIn").style.display = "none";
+            document.getElementById("loggedIn").style.display = "block";
+        })
+        .catch(err => {
+            alert(err);
+        })
+})
+
+
+loginClose.addEventListener("click", e => {
+    loginModal.style.display = "none";
+})
+
+registerClose.addEventListener("click", e => {
+    registerModal.style.display = "none";
+}
+)
+
+window.addEventListener("click", e => {
+    if (e.target == loginModal) {
+        loginModal.style.display = "none";
+    } else if (e.target == registerModal) {
+        registerModal.style.display = "none";
+    }
+})
+
+
